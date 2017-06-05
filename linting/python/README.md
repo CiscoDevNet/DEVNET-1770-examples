d72e04f7b61b59fb0c133975b5d22003cf1b11ca
Ashley Roach <asroach@cisco.com>
2017-06-05 16:48:35 -0600

    Add initial linting example

---
+++ linting/python/README.md
@@ -0,0 +1,5 @@
+
+- virtualenv venv
+- source venv/bin/activate
+- install pylint (pip install pylint)
+- apm install linter-pylint
---
+++ linting/python/example1.py
@@ -0,0 +1,14 @@
+
+
+"""This is an example of a long line"""
+print('This is a really, really, really, really, really, really, really, loooooong line.  Even longer.')
+
+
+def example():
+    """This is an example function with a docstring"""
+    print('This is an example')
+	print('Wait, there\'s a tab here')
+
+
+def example():
+    print('This is an example')
